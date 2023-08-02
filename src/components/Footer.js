11980a00b50312666b2252174add07906e173cc2
zohra-neda <zohra.neda@gmail.com>
2023-08-02 16:09:52 +0430

    Create components and tests directories

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+
+const Footer = () => (
+  <footer className="footer">
+    &copy; 2023 CryptoCurrency - Made by <a href="https://github.com/Zohra-Neda">Zohra Neda</a>
+  </footer>
+);
+
+export default Footer;
